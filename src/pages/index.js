edf0e7d95d0246bc1b6f5db619766426d18d2df9
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-10-22 23:20:23 -0400

    feat: Grouping by course

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import _ from "lodash"
 
 const IndexPage = ({ data }) => {
   const notes = data.allMarkdownRemark.edges
 
+  const groupedByClass = _.groupBy(notes, note => note.node.parent.relativeDirectory.split('/')[0])
+
   return (
     <div>
-      <h1>My Notes</h1>
-      <ul>
-        {notes.map(({ node }) => (
-          <li key={node.fields.slug}>
-            <Link to={node.fields.slug}>
-              {node.frontmatter.title || node.fields.slug}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1>My Notes by Class and Week</h1>
+      {Object.keys(groupedByClass).map(className => (
+        <div key={className}>
+          <h2>{className}</h2>
+          {Object.entries(_.groupBy(groupedByClass[className], note => note.node.parent.relativeDirectory.split('/')[1])).map(
+            ([weekName, weekNotes]) => (
+              <div key={weekName} style={{ marginLeft: '20px' }}>
+                <h3>{weekName}</h3>
+                <ul>
+                  {weekNotes.map(({ node }) => (
+                    <li key={node.fields.slug}>
+                      <Link to={node.fields.slug}>
+                        {node.frontmatter.title || node.fields.slug}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
+      ))}
     </div>
   )
 }
@@ -30,6 +45,11 @@ export const query = graphql`
           }
           fields {
             slug
+          }
+          parent {
+            ... on File {
+              relativeDirectory
+            }
           }
         }
       }
