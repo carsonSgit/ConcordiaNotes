0ebce52b1b5406bdfee188c58ba9c5b4da6fc48f
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-10-22 23:40:34 -0400

    feat: Styling and organization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,80 @@
-import React from "react"
-import { graphql, Link } from "gatsby"
-import styled from "styled-components"
-import _ from "lodash"
+import React from "react";
+import { graphql, Link } from "gatsby";
+import styled from "styled-components";
+import _ from "lodash";
 
 const Container = styled.div`
   padding: 2rem;
   font-family: Arial, sans-serif;
-`
+  border-radius: 8px;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #264653;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
 
 const ClassContainer = styled.div`
   margin-bottom: 2rem;
-`
+  padding: 1rem;
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
+`;
 
 const ClassTitle = styled.h2`
   font-size: 1.5rem;
   color: #2a9d8f;
-`
+  margin-bottom: 0.5rem;
+`;
 
 const WeekContainer = styled.div`
   margin-left: 20px;
   margin-bottom: 1.5rem;
-`
+`;
 
 const WeekTitle = styled.h3`
   font-size: 1.25rem;
   color: #264653;
-`
+  margin-bottom: 0.5rem;
+`;
 
 const NoteList = styled.ul`
   list-style-type: none;
   padding-left: 0;
-`
+`;
 
 const NoteItem = styled.li`
   margin: 0.5rem 0;
-  font-size: 1rem;
-`
+`;
 
 const NoteLink = styled(Link)`
   text-decoration: none;
-  color: #e76f51;
+  color: #1d8d55;
+  padding: 0.5rem;
+  border-radius: 4px;
+  transition: 0.2s ease-in-out;
 
   &:hover {
-    color: #f4a261;
-    text-decoration: underline;
+    background-color: #46b989;
+    color: #fff;
   }
-`
+`;
 
 const IndexPage = ({ data }) => {
-  const notes = data.allMarkdownRemark.edges
-  const groupedByClass = _.groupBy(notes, note => note.node.parent.relativeDirectory.split('/')[0])
+  const notes = data.allMarkdownRemark.edges;
+  const groupedByClass = _.groupBy(notes, note => note.node.parent.relativeDirectory.split('/')[0]);
 
   return (
     <Container>
-      <h1>Concordia Notes</h1>
+      <Title>Concordia Notes</Title>
       {Object.keys(groupedByClass).map(className => (
         <ClassContainer key={className}>
           <ClassTitle>{className}</ClassTitle>
-
           {Object.entries(_.groupBy(groupedByClass[className], note => note.node.parent.relativeDirectory.split('/')[1])).map(
             ([weekName, weekNotes]) => (
               <WeekContainer key={weekName}>
@@ -77,8 +94,8 @@ const IndexPage = ({ data }) => {
         </ClassContainer>
       ))}
     </Container>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
@@ -100,6 +117,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default IndexPage
+export default IndexPage;
--- src/templates/noteTemplate.js
+++ src/templates/noteTemplate.js
@@ -1,15 +1,49 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+
+// Styled Components
+const Container = styled.div`
+  padding: 2rem;
+  font-family: Arial, sans-serif;
+`
+
+const Title = styled.h1`
+  color: #264653;
+  margin-bottom: 1rem;
+`
+
+const Content = styled.div`
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #333;
+
+  a {
+    color: #1d8d55;
+
+    &:hover {
+      color: #46b989;
+    }
+  }
+
+  h2, h3, h4, h5, h6 {
+    color: #2a9d8f;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+`
 
 const NoteTemplate = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
 
   return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    <Container>
+      <Title>{frontmatter.title}</Title>
+      <Content dangerouslySetInnerHTML={{ __html: html }} />
+    </Container>
   )
 }
 
